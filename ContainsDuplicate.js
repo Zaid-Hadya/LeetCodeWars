31fb507c51e5772c4fdf66974ff77a451a465b61
zhadya <zaid.omar962@gmail.com>
2025-11-23 06:57:37 +0400

    solve the hasDublicate function with printing the elements

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -1,6 +1,6 @@
 class ContainsDuplicate {
-  // brute-force solution using iteration
-  hasDuplicate(nums) {
+  // brute-force solution using iteration.
+  hasDuplicate0 = (nums) => {
     let i = 0;
     let j = i + 1;
     while (i < nums.length) {
@@ -14,5 +14,27 @@ class ContainsDuplicate {
       j = i + 1;
     }
     return false;
-  }
+  };
+
+  // iteration solution with printing out the duplicated elements.
+  hasDuplicate1 = function (nums) {
+    let i = 0;
+    let j = i + 1;
+    let arr = [];
+    while (i < nums.length) {
+      while (j < nums.length) {
+        if (nums[i] === nums[j]) {
+          if (!arr.includes(nums[i])) arr.push(nums[i]);
+        }
+        j++;
+      }
+      i++;
+      j = i + 1;
+    }
+    return arr;
+  };
 }
+
+// for testing the code
+// const obj1 = new ContainsDuplicate();
+// console.log(obj1.hasDuplicate1([1, 3]));
