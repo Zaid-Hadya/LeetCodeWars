9c96c3f91f63fc36b6903df67a866443e40bb9e4
zhadya <zaid.omar962@gmail.com>
2025-12-03 22:41:36 +0400

    Solve by counting the freq of each char in the string

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -13,5 +13,15 @@ class Solution {
   }
 
   //  Count the frequancy of each char in the strings
-  isAnagram1(s, t) {}
+  isAnagram1(s, t) {
+    if (s.length !== t.length) return false;
+
+    const counts = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+      counts[s.charCodeAt(i) - 97]++;
+      counts[t.charCodeAt(i) - 97]--;
+    }
+    return counts.every((count) => count === 0);
+  }
 }
