7dc91f81219a3e648bc03b37b9a0663013e579eb
zhadya <zaid.omar962@gmail.com>
2025-12-05 11:46:57 +0400

    Solve the two sum function using nested loops

---
+++ TwoSum.js
@@ -0,0 +1,20 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  twoSum0(nums, target) {
+    let indices = [];
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target && i != j) {
+          indices.push(i);
+          indices.push(j);
+          return indices;
+        }
+      }
+    }
+    return nums;
+  }
+}
